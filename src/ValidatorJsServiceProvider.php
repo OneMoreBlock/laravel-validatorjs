0a331c2a31d5587aa3f4199eb50f5bea6ce8d79a
Ram Bhosale <rambhosale9@gmail.com>
2021-08-01 20:12:17 +0530

    feat : init new laravel package

---
+++ src/ValidatorJsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OneMoreBlock\Validatorjs;
+
+use Illuminate\Support\ServiceProvider;
+
+class ValidatorJsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
